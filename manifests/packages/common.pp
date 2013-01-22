cffc63fdb6999d888beb674430ef24546017441e
Daniel Barlow <daniel.barlow@simplybusiness.co.uk>
2013-01-22 11:44:12 +0000

    unfortunately, we need wget --no-check-certificate to downlaod from raw.github.com because their CDN gets in the way of hostname checking

--- manifests/packages/common.pp
+++ manifests/packages/common.pp
@@ -4,7 +4,9 @@ class rvm::packages::common($version='latest') {
   }
   
   exec { 'download-rvm-install':
-    command => 'wget -O /tmp/rvm https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer',
+    # --no-check-certificate is needed because otehrwise we get
+    # ERROR: certificate common name `*.a.ssl.fastly.net' doesn't match requested host name `raw.github.com'.
+    command => 'wget --no-check-certificate -O /tmp/rvm https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer',
     creates => '/tmp/rvm',
     unless  => 'which rvm',
   }
